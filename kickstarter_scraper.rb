89b14906273a6e2a403391f134b242b7ba428f1d
Ron Sala <ron.sala@gmail.com>
2018-02-18 11:19:24 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
-# percent funded: project.css("ul.project-stats li.first.funded strong").text.77"%", "").to_i
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')

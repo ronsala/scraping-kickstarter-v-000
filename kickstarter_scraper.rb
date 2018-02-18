e58a073a2bdd14032a7bee0e7f994b4fe94998a6
Ron Sala <ron.sala@gmail.com>
2018-02-18 10:44:04 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
+
 end
 
-binding.pry
+# binding.pry
 create_project_hash
